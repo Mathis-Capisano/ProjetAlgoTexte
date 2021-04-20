a94e37c9fe46d9f2d38e14e49b428dfa62a58c97
matthieutrinquart <81154540+matthieutrinquart@users.noreply.github.com>
2021-04-20 14:15:52 +0200

    Update ProjetAlgoFinal.java

--- ProjetAlgoFinal.java
+++ ProjetAlgoFinal.java
@@ -18,7 +18,7 @@ class JPasswordField2 implements ActionListener {
     Highlighter.HighlightPainter cyanPainter;
     Highlighter.HighlightPainter redPainter;
     JButton pickFile = new JButton("choisir fichier");
-    JButton transformButton = new JButton("transformer");
+    JButton transformButton = new JButton("remplacer");
     JButton saveButton = new JButton("Enregistrer");
     String emplacement;
     String  Text= "";
@@ -31,21 +31,18 @@ class JPasswordField2 implements ActionListener {
 
 
     public void init() {
-        JFrame f = new JFrame("ma fenetre");
+        JFrame f = new JFrame("KMP algo");
         f.setSize(800, 1600);
         JLabel l1,l2;
         JScrollPane texteAsc1;
         JScrollPane texteAsc2;
-        /*JPanel panneauTaille = new JPanel();
-        JPanel pannel = new JPanel();
-        JPanel panel = new JPanel();*/
+
         GridBagLayout layout = new GridBagLayout();
         f.setLayout(layout);
         GridBagConstraints gbc = new GridBagConstraints();
 
-        // Put constraints on different buttons
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        //Font fonte;
+
         passwordField1 = new JTextArea("");
         passwordField1.setPreferredSize(new Dimension(100, 25));
         passwordField2 = new JTextArea("");
@@ -58,7 +55,7 @@ class JPasswordField2 implements ActionListener {
         grandeZone2.setPreferredSize(new Dimension(100, 700));
         grandeZone2.setMaximumSize(new Dimension(100, 700));
         l1=new JLabel("Occurence");
-        l2=new JLabel("transformer en ");
+        l2=new JLabel("remplacer");
         gbc.gridx=0;
         gbc.gridy=0;
         f.add(l1,gbc);
@@ -78,9 +75,7 @@ class JPasswordField2 implements ActionListener {
         gbc.gridy=1;
         f.add(transformButton,gbc);
 
-        // fonte = new Font("TimesRoman", Font.PLAIN, 10);
-        //grandeZone1.setFont(fonte);
-        //grandeZone2.setFont(fonte);
+
         texteAsc1 = new JScrollPane(grandeZone1);
         texteAsc2 = new JScrollPane(grandeZone2);
         texteAsc1.setMinimumSize(new Dimension(100, 700));
@@ -91,7 +86,7 @@ class JPasswordField2 implements ActionListener {
         gbc.gridy=2;
         gbc.weightx = 1;
         gbc.weighty = 1;
-        // on attache la "fiche technique" au sous-composant
+
         f.add(texteAsc1,gbc);
         gbc.gridx=2;
         f.add(texteAsc2,gbc);
@@ -104,7 +99,6 @@ class JPasswordField2 implements ActionListener {
         f.add(saveButton,gbc);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // f.getContentPane().add(pannel);
         f.setVisible(true);
     }
 
@@ -113,23 +107,18 @@ class JPasswordField2 implements ActionListener {
         if (source.equals(pickFile)) {
             File repertoireCourant = null;
             try {
-                // obtention d'un objet File qui désigne le répertoire courant. Le
-                // "getCanonicalFile" n'est pas absolument nécessaire mais permet
-                // d'éviter les /Truc/./Chose/ ...
+
                 repertoireCourant = new File(".").getCanonicalFile();
-                System.out.println("Répertoire courant : " + repertoireCourant);
             } catch (IOException e1) {
             }
 
-            // création de la boîte de dialogue dans ce répertoire courant
-            // (ou dans "home" s'il y a eu une erreur d'entrée/sortie, auquel
-            // cas repertoireCourant vaut null)
+
             JFileChooser dialogue = new JFileChooser(repertoireCourant);
 
-            // affichage
+
             dialogue.showOpenDialog(null);
 
-            // récupération du fichier sélectionné
+
             emplacement = dialogue.getSelectedFile().getPath() ;
 
                 try {
@@ -148,9 +137,7 @@ class JPasswordField2 implements ActionListener {
         if (source.equals(transformButton)) {
             grandeZone2.getHighlighter().removeAllHighlights();
             grandeZone1.getHighlighter().removeAllHighlights();
-            System.out.println("C'est moi " + KMP(grandeZone1.getText(), passwordField1.getText(), passwordField2.getText()));
 
-            System.out.println("C'est moi 2" + dest);
             grandeZone2.setText(KMP(grandeZone1.getText(), passwordField1.getText(), passwordField2.getText()));
 
             for (int i = 0; i < liste1.size(); ++i) {
@@ -176,13 +163,10 @@ class JPasswordField2 implements ActionListener {
 
 
             }
-            System.out.println(enregistrer);
             if (enregistrer != "") {
                 String nom = JOptionPane.showInputDialog(null, "nom du fichier :");
-                System.out.println(nom);
                 if (nom != null) {
                     enregistrer = enregistrer + nom;
-                    System.out.println(enregistrer);
 
                     try {
                         enregistrer(enregistrer);
@@ -204,7 +188,6 @@ class JPasswordField2 implements ActionListener {
             // Afficher le contenu du fichier
             Text = Text +'\n' + line;
         }
-        System.out.println(Text);
         in.close();
 
     }
@@ -213,22 +196,18 @@ class JPasswordField2 implements ActionListener {
         int j = 0;
         int nbOccurences = 0;
         int differenceTaille = remplace.length() - occurence.length();
-        boolean fini = true;
+        boolean fini ;
         for(int i =0 ; i < dest.length() ; ++i) {
             fini = true;
             while(fini){
-                System.out.println(j);
                 if(dest.charAt(i+j) == occurence.charAt(j)) {
                     if(j == occurence.length()-1) {
                         int index = i;
                         String cible = dest.substring(index, i+(occurence.length()));
-                        System.out.println(cible);
                         if ( differenceTaille > 0 ) {
-                            System.out.println((index-(differenceTaille*nbOccurences))+" / "+(index-(differenceTaille*nbOccurences) + cible.length()));
                             int[] tab1 = {index-(differenceTaille*nbOccurences),index-(differenceTaille*nbOccurences) + cible.length()};
                             liste1.add(tab1);
                         } else if ( differenceTaille < 0 ) {
-                            System.out.println((index+Math.abs(differenceTaille*nbOccurences))+" / "+(index+Math.abs(differenceTaille*nbOccurences) + cible.length()));
                             int[] tab1 = {index+Math.abs(differenceTaille*nbOccurences),index+Math.abs(differenceTaille*nbOccurences) + cible.length()};
                             liste1.add(tab1);
                         } else {
@@ -237,7 +216,7 @@ class JPasswordField2 implements ActionListener {
                         }
                         replaceAndHighlight(cible,index,remplace);
                         nbOccurences++;
-
+                        i = i + remplace.length()-1;
                         j=0;
                         fini = false;
 
@@ -258,97 +237,14 @@ class JPasswordField2 implements ActionListener {
         }
         return dest;
     }
-    /*
-    public static String KMP(String chaine , String occurence , String remplace){
-        dest = chaine;
-        int j=0;
-        int nbOccurences = 0;
-        int differenceTaille = remplace.length() - occurence.length();
-        for(int i =0 ; i < dest.length() ; ++i){
-            if(dest.charAt(i) == occurence.charAt(j)){
-                if(j == occurence.length()-1){
-                    int index = i-j;
-                    String cible = dest.substring(index, i+1);
-
-                    //System.out.println("cible: "+ cible);
-                    System.out.println("nbOccurences: "+ nbOccurences+ " index: "+index+ " differenceTaille: "+ differenceTaille);
-                    if ( differenceTaille > 0 ) {
-                        System.out.println((index-(differenceTaille*nbOccurences))+" / "+(index-(differenceTaille*nbOccurences) + cible.length()));
-                        int[] tab1 = {index-(differenceTaille*nbOccurences),index-(differenceTaille*nbOccurences) + cible.length()};
-                        liste1.add(tab1);
-                    } else if ( differenceTaille < 0 ) {
-                        System.out.println((index+Math.abs(differenceTaille*nbOccurences))+" / "+(index+Math.abs(differenceTaille*nbOccurences) + cible.length()));
-                        int[] tab1 = {index+Math.abs(differenceTaille*nbOccurences),index+Math.abs(differenceTaille*nbOccurences) + cible.length()};
-                        liste1.add(tab1);
-                    } else {
-                        int[] tab1 = {index,index + cible.length()};
-                        liste1.add(tab1);
-                    }
-                    replaceAndHighlight(cible,index,remplace);
-                    i += remplace.length();
-                    j = 0 ;
-
-                    nbOccurences++;
-
-                }
-                else
-                    ++j;
-            }
-            else{
-                j = 0 ;
-            }
-        }
-        return dest;
 
 
-    }
-*/
-    public static String KMP2(String chaine , String occurence , String remplace){
-        dest = chaine;
-        int j = 0;
-        boolean fini = true;
-        for(int i =0 ; i < dest.length() ; ++i) {
-            fini = true;
-            while(fini){
-                System.out.println(j);
-                if(dest.charAt(i+j) == occurence.charAt(j)) {
-                    if(j == occurence.length()-1) {
-                        int index = i;
-                        String cible = dest.substring(index, i+(occurence.length()));
-                        System.out.println(cible);
-                        replaceAndHighlight(cible,index,remplace);
-
-                        j=0;
-                        fini = false;
-
-                    }
-                    else {
-                        ++j;
-                    }
-
-
-                }
-                else{
-                    j = 0;
-                    fini = false;
-                }
-
-            }
-
-        }
-        return dest;
-    }
-    /*
-     * cible: texte initial
-     * index: index de commencement de la cible
-     * motif: texte de remplacement
-     */
     public static void replaceAndHighlight(String cible, int index, String motif) {
 
         String partA = dest.substring(0, index) ;
-        //      System.out.println(partA);
+
         String partB = motif;
-        //System.out.println(partB);
+
 
         int borne = index + cible.length() ;
 
@@ -358,7 +254,7 @@ class JPasswordField2 implements ActionListener {
         }
 
         String partC = dest.substring(borne, dest.length()) ;
-        //System.out.println(partC);
+
         int[] tab2 = {index,index +motif.length()};
         liste2.add(tab2);
         dest =  partA + partB + partC;
@@ -373,11 +269,10 @@ class JPasswordField2 implements ActionListener {
     public  void enregistrer(String emplacement) throws IOException {
         try {
 
-            // Recevoir le fichier
+
             File f = new File(emplacement+".txt");
 
-            // Créer un nouveau fichier
-            // Vérifier s'il n'existe pas
+
             if (f.createNewFile())
                 System.out.println("File created");
             else
